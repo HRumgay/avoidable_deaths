4c312b72df5b89a545ba745ddfb5b8da6bbdc91d
HRumgay <30409755+HRumgay@users.noreply.github.com>
2022-06-21 10:17:00 +0200

    Added example code for two-sided stacked bar chart

--- AD_2_Figures + Tables.R
+++ AD_2_Figures + Tables.R
@@ -1,3 +1,10 @@
+# load libraries needed for some charts
+library(tidyverse)
+library(data.table)
+library(Rcan)
+
+
+
 # Avoidable_Deaths2 <- Avoidable_Deaths %>% mutate(Scenario = "RWD")%>%
 #   select(-cancer)
 #   
@@ -188,12 +195,14 @@ ggsave("CHARTS/bars/AD_barplot_prev.png",AD_barplot_prev, height=10, width=10)
 ggsave("CHARTS/bars/AD_barplot_unavoid.png",AD_barplot_unavoid, height=10, width=10)
 
 
+#--- code for horizontal two-sided stacked bar charts
+#convert data to long format
 AD_plotable %>% 
   pivot_longer(cols = starts_with("AD_"),
                names_to = "type",
                values_to = "deaths") -> AD_plotable_L
 
-
+# create stacked bar for overall ages combined
 AD_plotable_L %>% 
   filter(age_cat=="Overall", type %in% c("AD_treat", "AD_prev", "AD_unavoid")) %>% 
   ggplot(
@@ -221,7 +230,7 @@ AD_plotable_L %>%
   )
 ggsave("CHARTS/bars/AD_barplot_overall.pdf", height=10, width=10)
 
-
+# create stacked bar for age 15-64 
 AD_plotable_L %>% 
   filter(age_cat=="15-64", type %in% c("AD_treat", "AD_prev", "AD_unavoid")) %>% 
   ggplot(
@@ -249,6 +258,7 @@ AD_plotable_L %>%
   )
 ggsave("CHARTS/bars/AD_barplot_1564.pdf", height=10, width=10)
 
+# create stacked bar for age 65-99
 AD_plotable_L %>% 
   filter(age_cat=="65-99", type %in% c("AD_treat", "AD_prev", "AD_unavoid")) %>% 
   ggplot(
@@ -278,5 +288,54 @@ ggsave("CHARTS/bars/AD_barplot_6599.pdf", height=10, width=10)
 
 
 
+#--- example code for horizontal two-sided stacked bar charts ----
+#make sure data are in long format for stacked bars
+#in this code the variable 'level' is being stacked, so could change this to scenario (prev, treat, unavoid) or age?
+#you could also make this by cancer site instead of by world region
 
+# produce horizontal bars split males and females by world region
+# create dual bar chart, males on left, females on right
+pafsplit_subregion2 %>% 
+  mutate(pafplot = case_when(sex == 1 ~ grptotpaf * -1,  #create negative values to plot left of y axis
+                             sex == 2 ~ grptotpaf)) -> pafsplit_subregion2t
+pafsplit_subregion2t <- as.data.table(pafsplit_subregion2t)
+#order world regions by proportion or ASR
+dt_label_order3 <- setkeyv(unique(pafs_subregion2[cancer_label == "All cancers but non-melanoma skin cancer_M" & sex==1, c("subregion_label_2", "grptotpaf"), with=FALSE]), c("grptotpaf"))
+pafsplit_subregion2t$subregion_label_2 <- factor(pafsplit_subregion2t$subregion_label_2, levels = unique(dt_label_order3$subregion_label_2,fromLast=TRUE))
+
+#remove unnecessary rows
+pafsplit_subregion2t %>% 
+  filter(sex!=0) %>% 
+  dplyr::select(-age_num, -grpaacasesage, -grppyage) %>% unique() %>% 
+  filter(cancer_label=="All cancers but non-melanoma skin cancer_M", level<4) -> pafsplit_subregion2t
+
+#create lists of labels for axes
+tick_minor_list <- Rcan:::core.csu_tick_generator(max = max(pafs_subregion2[cancer_label == "All cancers but non-melanoma skin cancer_M" & sex==1]$grptotpaf, na.rm = TRUE ), 0)$tick_list
+nb_tick <- length(tick_minor_list) 
+tick_space <- tick_minor_list[nb_tick] - tick_minor_list[nb_tick-1]
+if ((tick_minor_list[nb_tick] -  max(pafs_subregion2[cancer_label == "All cancers but non-melanoma skin cancer_M" & sex==1]$grptotpaf))/tick_space < 1/4){
+  tick_minor_list[nb_tick+1] <- tick_minor_list[nb_tick] + tick_space
+}
+tick_major <- tick_minor_list[1:length(tick_minor_list) %% 2  == 1]
+tick_major_list <- c(rev(-tick_major),tick_major[tick_major!=0])
+tick_label <- c(rev(tick_major),tick_major[tick_major!=0])
+tick_minor_list <- c(rev(-tick_minor_list),tick_minor_list[tick_minor_list!=0])
+
+#create two-sided stacked bar by world region and sex
+pafsplit_subregion2t %>% 
+  mutate(sex = case_when(sex==1 ~ "Male", sex==2~"Female"),
+         level = case_when(level==1 ~ "<20",level==2 ~ "20-60", level==3~">60")) %>% 
+  ggplot(aes(x=subregion_label_2, y=pafplot, fill=factor(level, levels = c(">60","20-60","<20"))))+ 
+  geom_bar(stat = 'identity', colour = 'black', size = .1)+
+  geom_hline(yintercept = 0, size=1)+
+  coord_flip(ylim = c(tick_minor_list[1]-(tick_space*0.25),tick_minor_list[length(tick_minor_list)]+(tick_space*0.25)), expand = FALSE)+
+  scale_fill_manual(values = c('#de2d26','#fc9272',  '#fee0d2')) +
+  labs(x = 'World Region', y = 'Population Attributable Fraction (%)', fill = 'Consumption level\n         (g/day)') +
+  scale_y_continuous(breaks=tick_major_list,
+                     minor_breaks = tick_minor_list,
+                     labels=scales::percent(tick_label, accuracy=1))+
+  theme_minimal()+
+  theme(legend.position = 'bottom', axis.text.x=element_text(angle=90,hjust=1, vjust = .5)) + 
+  guides(fill=guide_legend(reverse = TRUE))
+ggsave("levels_subregion2_mf_horiz.pdf", width = 15, height = 10)
 
