79a8289cf8c712c48c22d93923f6b8b1c54e0487
olangs <oliver.langselius@gmail.com>
2022-07-27 10:28:38 +0200

    Update AD_Main_Figures.R

--- AD Preventable and Treatable/AD_Main_Figures.R
+++ AD Preventable and Treatable/AD_Main_Figures.R
@@ -42,11 +42,13 @@ AD_by_cancer_site_1<-AD_by_cancer_site%>%
 # Pie Chart
 
 Figure_4_1 <- AD_by_cancer_site_1%>%
+  filter(AD_cat==)
   ungroup()%>%
   mutate(wght = runif(length(AD)))%>%
-  #mutate(wght = wght/sum(wght))%>%
+  mutate(wght = wght/sum(wght))%>%
   mutate(pos = (cumsum(c(0, wght)) + c(wght / 2, .01))[1:nrow(AD_by_cancer_site_1)])%>%
   group_by(AD_cat)%>%
+  distinct()
   arrange(desc(AD), .by_group = TRUE) %>%
   ggplot(aes(x="", y=wght, fill = AD_cat)) +
   geom_col(color = 'black', 
@@ -55,12 +57,14 @@ Figure_4_1 <- AD_by_cancer_site_1%>%
   geom_text_repel(aes(x = 1.4, y = pos, label = cancer), 
                   nudge_x = .3, 
                   segment.size = .7, 
-                  show.legend = TRUE) +
+                  show.legend = TRUE,max.overlaps = 42) +
   scale_fill_discrete(name = "Type of avoidable deaths", 
                       labels = c("Preventable", "Treatable", "Unavoidable")) +
   coord_polar("y", start=0) +
   labs(title="Preventable and treatable avoidable deaths globally by cancer site")+
   theme_void()
+
+Figure_4_1
 
 # Barplot
 
